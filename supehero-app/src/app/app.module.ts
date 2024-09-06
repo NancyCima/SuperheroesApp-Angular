6c23cde32c32ce26ccec951b8d33ce210728002b
Nancy <nancy.cima.bertoni@gmail.com>
2024-09-06 10:15:18 -0300

    create: nav bar component

--- supehero-app/src/app/app.module.ts
+++ supehero-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AddHeroComponent } from './add-hero/add-hero.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { SearchHeroComponent } from './search-hero/search-hero.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddHeroComponent,
     HeroListComponent,
-    SearchHeroComponent
+    SearchHeroComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
