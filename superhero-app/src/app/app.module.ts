bf4ea4cf2c8a433cc7f70885cc79bb13d04d3c56
paguero8 <paguero@asap-consulting.net>
2024-09-06 10:50:53 -0300

    changing folder names

--- superhero-app/src/app/app.module.ts
+++ superhero-app/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddHeroComponent } from './add-hero/add-hero.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
-import { SearchHeroComponent } from './search-hero/search-hero.component';
+import { AddHeroComponent } from './superhero/components/add-hero/add-hero.component';
+import { HeroListComponent } from './superhero/components/hero-list/hero-list.component';
+import { SearchHeroComponent } from './superhero/components/search-hero/search-hero.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
