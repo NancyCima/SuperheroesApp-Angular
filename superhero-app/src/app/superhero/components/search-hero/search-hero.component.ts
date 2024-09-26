38f3430ab9ab14182520202adf99d784d5121eb7
Nancy <nancy.cima.bertoni@gmail.com>
2024-09-26 16:49:47 -0300

    add: code for searching heroes

--- superhero-app/src/app/superhero/components/search-hero/search-hero.component.ts
+++ superhero-app/src/app/superhero/components/search-hero/search-hero.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-hero',
+  standalone: true,
   templateUrl: './search-hero.component.html',
-  styleUrls: ['./search-hero.component.css']
+  styleUrls: ['./search-hero.component.css'],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule],
 })
 export class SearchHeroComponent {
+  searchTerm: string = '';
 
+  @Output() searchHero = new EventEmitter<string>();
+
+  // Emitir el término de búsqueda hacia el componente padre
+  onSearch() {
+    this.searchHero.emit(this.searchTerm);
+  }
 }
--- superhero-app/src/app/superhero/pages/search-page/search-page.component.ts
+++ superhero-app/src/app/superhero/pages/search-page/search-page.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SuperHeroesService } from '../../services/superheroes.services';
+import { SearchHeroComponent } from '../../components/search-hero/search-hero.component';
 
 @Component({
   selector: 'app-search-page',
-  templateUrl: './search-page.component.html'
+  standalone: true,
+  templateUrl: './search-page.component.html',
+  styleUrls: ['./search-page.component.css'],
+  imports: [CommonModule, SearchHeroComponent]
 })
 export class SearchPageComponent {
+  loading = false;
+  error: string | null = null;
+  filteredHeros: any[] = [];
 
+  constructor(private heroService: SuperHeroesService) {}
+
+  // Este método se activa cuando el hijo emite el término de búsqueda
+  onSearchHero(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const term = input.value;
+    this.loading = true;
+    this.error = '';
+    
+    this.heroService.searchHero(term).subscribe(
+      (data) => {
+        this.filteredHeros = data;
+        this.loading = false;
+      },
+      (error) => {
+        this.error = 'Error al obtener heroínas. Inténtalo de nuevo más tarde.';
+        this.loading = false;
+      }
+    );
+  }
 }
