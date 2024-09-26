51ffb3c3a985579c8ac8f20a9b8c06aae6ac6415
Nancy <nancy.cima.bertoni@gmail.com>
2024-09-26 10:35:18 -0300

    add: background image and style to the 404 error page

--- superhero-app/src/app/shared/pages/error404-page/error404-page.component.ts
+++ superhero-app/src/app/shared/pages/error404-page/error404-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error404-page',
   templateUrl: './error404-page.component.html',
-  styles: ``
+  styleUrls: ['./error404-page.component.css']
 })
 export class Error404PageComponent {
+  constructor(private router: Router) {}
 
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
