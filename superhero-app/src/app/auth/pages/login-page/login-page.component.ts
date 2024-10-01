26dddfb59f2f53cfe518ccefac111bfeaec2cb7b
paguero8 <paguero@asap-consulting.net>
2024-10-01 17:48:30 -0300

    adding snack bar

--- superhero-app/src/app/auth/pages/login-page/login-page.component.ts
+++ superhero-app/src/app/auth/pages/login-page/login-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.services';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css',
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
   loginForm: FormGroup;
@@ -14,7 +15,8 @@ export class LoginPageComponent {
   constructor(
     private authService: AuthService,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -37,15 +39,37 @@ export class LoginPageComponent {
       this.authService.login(email, password).subscribe(
         (isAuthenticated: boolean) => {
           if (isAuthenticated) {
-            console.log('Inicio correcto');
+            this.snackBar.open('Inicio de sesión correcto', 'Cerrar', {
+              duration: 3000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center',
+            });
             this.router.navigate(['/home']);
           } else {
-            console.log('error');
-            alert('Correo incorrecto');
+            this.snackBar.open('Credenciales incorrectas', 'Cerrar', {
+              duration: 3000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center',
+            });
           }
         },
         (error: any) => {
+          this.snackBar.open('Error durante la autenticación', 'Cerrar', {
+            duration: 3000,
+            verticalPosition: 'top',
+            horizontalPosition: 'center',
+          });
           console.error('Error durante la autenticación', error);
+        }
+      );
+    } else {
+      this.snackBar.open(
+        'Por favor complete el formulario correctamente',
+        'Cerrar',
+        {
+          duration: 3000,
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
         }
       );
     }
--- superhero-app/src/app/auth/pages/sing-up-page/sing-up-page.component.ts
+++ superhero-app/src/app/auth/pages/sing-up-page/sing-up-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.services';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-sing-up-page',
   templateUrl: './sing-up-page.component.html',
-  styleUrl: './sing-up-page.component.css'
+  styleUrls: ['./sing-up-page.component.css'],
 })
 export class SingUpPageComponent {
   registerForm: FormGroup;
@@ -14,51 +15,78 @@ export class SingUpPageComponent {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService
-  ){
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {
     this.registerForm = this.fb.group(
       {
         name: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
         password: ['', Validators.required],
-        confirmPassword: ['', Validators.required]
+        confirmPassword: ['', Validators.required],
       },
-      { validator: this.passwordMatchValidator}
+      { validator: this.passwordMatchValidator }
     );
   }
 
   passwordMatchValidator(form: FormGroup) {
     return form.get('password')?.value === form.get('confirmPassword')?.value
-    ? null
-    : { mismatch: true};
+      ? null
+      : { mismatch: true };
   }
 
   onSubmit(): void {
     if (this.registerForm.valid) {
-      const { name, email, password} = this.registerForm.value;
+      const { name, email, password } = this.registerForm.value;
 
       this.authService.checkEmailExists(email).subscribe(
         (emailExists) => {
           if (emailExists) {
-            alert('El correo ya se registro ')
+            this.snackBar.open('El correo ya se registró', 'Cerrar', {
+              duration: 3000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center',
+            });
           } else {
             this.authService.registerUser(name, email, password).subscribe(
               () => {
-                alert('Usuario registrado');
+                this.snackBar.open('Usuario registrado', 'Cerrar', {
+                  duration: 3000,
+                  verticalPosition: 'top',
+                  horizontalPosition: 'center',
+                });
                 this.router.navigate(['/auth/login']);
               },
               (error) => {
-                console.error('error');
-                alert('error')
+                console.error('Error al registrar usuario', error);
+                this.snackBar.open('Error al registrar usuario', 'Cerrar', {
+                  duration: 3000,
+                  verticalPosition: 'top',
+                  horizontalPosition: 'center',
+                });
               }
             );
           }
         },
         (error) => {
           console.error('Error al verificar usuario', error);
+          this.snackBar.open('Error al verificar el correo', 'Cerrar', {
+            duration: 3000,
+            verticalPosition: 'top',
+            horizontalPosition: 'center',
+          });
+        }
+      );
+    } else {
+      this.snackBar.open(
+        'Por favor complete el formulario correctamente',
+        'Cerrar',
+        {
+          duration: 3000,
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
         }
       );
     }
-  }  
-
+  }
 }
